dd3e9eac7424929eff372e97f4fdd70cb9963408
Mustafa Raza <mustafaraza@Mustafas-MacBook-Pro.local>
2024-04-02 14:05:02 +0500

    login Screen ui done

--- lib/App Modules/Auth/View/login_screen.dart
+++ lib/App Modules/Auth/View/login_screen.dart
@@ -7,31 +7,211 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
+      backgroundColor: Colors.white,
+      body: Container(
           height: double.infinity,
           width: double.infinity,
+          color: const Color.fromARGB(255, 1, 47, 1),
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              // mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                const SizedBox(height: 50),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                    border: Border.all(
+                      color: Colors.yellowAccent,
+                      width: 5,
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  child: CustomText(
+                    title: "Denise Perfume",
+                    color: Colors.white.withOpacity(1),
+                    fontSize: 35,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const CustomText(
+                  title: "Login",
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 20),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: CustomText(
+                    title: "Email",
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 10),
                 TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Email",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    disabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.pink,
+                      ),
+                    ),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    hintText: "Enter Email Address",
+                    hintStyle: TextStyle(
+                      color: Colors.white.withOpacity(0.5),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: CustomText(
+                    title: "Password",
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    disabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.pink,
+                      ),
+                    ),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    hintText: "Enter your Password",
+                    hintStyle: TextStyle(
+                      color: Colors.white.withOpacity(0.5),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Align(
+                  alignment: Alignment.centerRight,
+                  child: CustomText(
+                    title: "Forgot Password",
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
                 const SizedBox(height: 30),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Password",
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.amber,
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.green,
+                    ),
+                  ),
+                  child: const Center(
+                    child: CustomText(
+                      title: "Login",
+                      color: Colors.red,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const CustomText(
-                    title: "Login",
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomText(
+                      title: "Create New Account",
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    const CustomText(
+                      title: " ( ",
+                      color: Colors.pink,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print(" Event Call Successfully");
+                      },
+                      child: Container(
+                        color: Colors.transparent,
+                        child: const CustomText(
+                          title: "Sign up",
+                          color: Colors.yellowAccent,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const CustomText(
+                      title: " )",
+                      color: Colors.pink,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ],
                 )
               ],
             ),
